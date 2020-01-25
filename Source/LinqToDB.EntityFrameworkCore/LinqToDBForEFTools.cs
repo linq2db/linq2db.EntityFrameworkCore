e2dc32444038844084efd3e919e311c1a026de4b
MaceWindu <MaceWindu@users.noreply.github.com>
2020-01-25 15:42:53 +0100

    fixes

--- Source/LinqToDB.EntityFrameworkCore/LinqToDBForEFTools.cs
+++ Source/LinqToDB.EntityFrameworkCore/LinqToDBForEFTools.cs
@@ -119,6 +119,7 @@ namespace LinqToDB.EntityFrameworkCore
 		/// </summary>
 		/// <param name="model">EF.Core data model instance. Could be <c>null</c>.</param>
 		/// <param name="dependencies"></param>
+		/// <param name="mappingSource"></param>
 		/// <returns>LINQ To DB metadata provider.</returns>
 		public static IMetadataReader GetMetadataReader(
 			IModel? model,
