0d99681400cf6e4b9f7b77fb1bf4b1555c7120d8
Svyatoslav Danyliv <sdanyliv@gmail.com>
2020-01-30 16:15:36 +0200

    Added tests for value conversion.

---
+++ Tests/LinqToDB.EntityFrameworkCore.Tests/ValueConversion/ConvertorTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using NUnit.Framework;
+
+namespace LinqToDB.EntityFrameworkCore.Tests.ValueConversion
+{
+	[TestFixture]
+	public class ConvertorTests
+	{
+		private DbContextOptions<ConvertorContext> _options;
+
+		public class ConvertorContext : DbContext
+		{
+			public ConvertorContext(DbContextOptions options) : base(options)
+			{
+			}
+
+			public virtual DbSet<SubDivision> Subdivisions { get; set; }
+		}
+
+		public ConvertorTests()
+		{
+			var optionsBuilder = new DbContextOptionsBuilder<ConvertorContext>();
+
+			optionsBuilder
+				.ReplaceService<IValueConverterSelector, IdValueConverterSelector>()
+				.UseSqlServer("Server=.;Database=ConverterTests;Integrated Security=SSPI")
+				.UseLoggerFactory(TestUtils.LoggerFactory);;
+
+			_options = optionsBuilder.Options;
+		}
+
+
+		[Test]
+		public void TestToList()
+		{
+			using (var ctx = new ConvertorContext(_options))
+			using (var db = ctx.CreateLinqToDbConnection())
+			{
+				ctx.Database.EnsureDeleted();
+				ctx.Database.EnsureCreated();
+
+				ctx.Subdivisions.Add(new SubDivision()
+					{ Code = "C1", Id = new Id<SubDivision, long>(1), Name = "N1", PermanentId = Guid.NewGuid() });
+
+				ctx.SaveChanges(true);
+
+				var ef   = ctx.Subdivisions.Where(s => s.Id == 1).ToArray();
+				var ltdb = ctx.Subdivisions.ToLinqToDB().Where(s => s.Id == 1).ToArray();
+			}
+		}
+
+	}
+}
