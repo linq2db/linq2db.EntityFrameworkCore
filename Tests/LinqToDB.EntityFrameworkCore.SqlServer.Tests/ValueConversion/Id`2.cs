68fbea1d3d423f231fd78581c53178cf74aba9ec
Bonart <leo.bonart@gmail.com>
2020-05-02 15:33:32 +0300

    Use semantic Id<>.ToString()

--- Tests/LinqToDB.EntityFrameworkCore.SqlServer.Tests/ValueConversion/Id`2.cs
+++ Tests/LinqToDB.EntityFrameworkCore.SqlServer.Tests/ValueConversion/Id`2.cs
@@ -8,7 +8,7 @@ namespace LinqToDB.EntityFrameworkCore.SqlServer.Tests.ValueConversion
 		public Id(TKey id) => Value = id;
 		public TKey Value { get; }
 		public static implicit operator TKey(Id<TEntity, TKey> id) => id.Value;
-		public override string ToString() => Value?.ToString();
+		public override string ToString() => $"{typeof(TEntity).Name}({Value})";
 		public bool Equals(Id<TEntity, TKey> other) => EqualityComparer<TKey>.Default.Equals(Value, other.Value);
 		public override bool Equals(object obj) => obj is Id<TEntity, TKey> other && Equals(other);
 		public override int GetHashCode() => EqualityComparer<TKey>.Default.GetHashCode(Value);
