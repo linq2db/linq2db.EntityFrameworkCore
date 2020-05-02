aadda0d83b2f557db9adb22afdc9a1c11e3ca54c
Bonart <leo.bonart@gmail.com>
2020-05-02 15:34:14 +0300

    Add helper methods for Id<>

--- Tests/LinqToDB.EntityFrameworkCore.SqlServer.Tests/ValueConversion/Id`2.cs
+++ Tests/LinqToDB.EntityFrameworkCore.SqlServer.Tests/ValueConversion/Id`2.cs
@@ -3,6 +3,16 @@ using System.Collections.Generic;
 
 namespace LinqToDB.EntityFrameworkCore.SqlServer.Tests.ValueConversion
 {
+	public static class Id
+	{
+		public static Id<T, long> AsId<T>(this long id) where T : IEntity<long> => id.AsId<T, long>();
+
+		public static Id<T, Guid> AsId<T>(this Guid guid) where T : IEntity<Guid> => guid.AsId<T, Guid>();
+        
+		public static Id<TEntity, TKey> AsId<TEntity, TKey>(this TKey @object) where TEntity : IEntity<TKey> 
+			=> new Id<TEntity, TKey>(@object);
+	}
+	
 	public readonly struct Id<TEntity, TKey> : IEquatable<Id<TEntity, TKey>> where TEntity : IEntity<TKey>
 	{
 		public Id(TKey id) => Value = id;
