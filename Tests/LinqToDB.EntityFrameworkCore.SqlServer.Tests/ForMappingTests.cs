f0ca92ad6cdcce6f2111e7932881455ea1a6c886
Svyatoslav Danyliv <sdanyliv@gmail.com>
2021-07-08 14:42:08 +0300

    Fix for #157. Improved DataType handling. (#159)
    * Improved types detection.
    
    * Improved tests.

--- Tests/LinqToDB.EntityFrameworkCore.SqlServer.Tests/ForMappingTests.cs
+++ Tests/LinqToDB.EntityFrameworkCore.SqlServer.Tests/ForMappingTests.cs
@@ -1,4 +1,6 @@
-﻿using LinqToDB.EntityFrameworkCore.BaseTests;
+﻿using System.Linq;
+using FluentAssertions;
+using LinqToDB.EntityFrameworkCore.BaseTests;
 using LinqToDB.EntityFrameworkCore.BaseTests.Models.ForMapping;
 using LinqToDB.EntityFrameworkCore.SqlServer.Tests.Models.ForMapping;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +11,8 @@ namespace LinqToDB.EntityFrameworkCore.SqlServer.Tests
 	[TestFixture]
 	public class ForMappingTests : ForMappingTestsBase
 	{
+		private bool _isDbCreated;
+
 		public override ForMappingContextBase CreateContext()
 		{
 			var optionsBuilder = new DbContextOptionsBuilder<ForMappingContext>();
@@ -18,10 +22,32 @@ namespace LinqToDB.EntityFrameworkCore.SqlServer.Tests
 			var options = optionsBuilder.Options;
 			var ctx = new ForMappingContext(options);
 
-			//ctx.Database.EnsureDeleted();
-			ctx.Database.EnsureCreated();
+			if (!_isDbCreated)
+			{
+				ctx.Database.EnsureDeleted();
+				ctx.Database.EnsureCreated();
+
+				_isDbCreated = true;
+			}
 
 			return ctx;
+		}
+
+		[Test]
+		public void TestStringMappings()
+		{
+			using (var db = CreateContext())
+			{
+				var ms = LinqToDBForEFTools.GetMappingSchema(db.Model, db);
+				var ed = ms.GetEntityDescriptor(typeof(StringTypes));
+
+				ed.Columns.First(c => c.MemberName == nameof(StringTypes.AnsiString)).DataType.Should()
+					.Be(DataType.VarChar);
+
+				ed.Columns.First(c => c.MemberName == nameof(StringTypes.UnicodeString)).DataType.Should()
+					.Be(DataType.NVarChar);
+			}
+
 		}
 	}
 }
