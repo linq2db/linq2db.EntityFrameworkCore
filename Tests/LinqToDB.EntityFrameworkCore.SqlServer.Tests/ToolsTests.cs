3624cbcfdaab0f217a9ef92b5869f174654d980a
Svyatoslav Danyliv <sdanyliv@gmail.com>
2021-08-04 14:55:37 +0300

    Fixed SqlServer tests.

--- Tests/LinqToDB.EntityFrameworkCore.SqlServer.Tests/ToolsTests.cs
+++ Tests/LinqToDB.EntityFrameworkCore.SqlServer.Tests/ToolsTests.cs
@@ -370,7 +370,9 @@ namespace LinqToDB.EntityFrameworkCore.SqlServer.Tests
 		{
 			using (var ctx = CreateContext(enableFilter))
 			{
-				var query = ctx.Products.AsQueryable().Where(p => p.ProductName.Contains('a'));
+#pragma warning disable CA1847 // Use char literal for a single character lookup
+				var query = ctx.Products.AsQueryable().Where(p => p.ProductName.Contains("a"));
+#pragma warning restore CA1847 // Use char literal for a single character lookup
 
 				var expectedArray = await query.ToArrayAsync();
 				var expectedDictionary = await query.ToDictionaryAsync(p => p.ProductId);
