d2bd0d3321d50e033d9351778f404c021439ca02
MaceWindu <MaceWindu@users.noreply.github.com>
2022-11-13 10:04:40 +0100

    Added possibility to define default interceptors for Linq2Db. (#259) (#269)
    * Added possibility to define default interceptors for Linq2Db.
    
    * CR fixes
    
    * changed the way of Linq2Db Interceptors registration. Made it possible to use interceptors registered for EF core also.
    
    * improved existing interceptors checking logic. Naming changes inside interceptor tests.
    
    * cr fix: removed duplicated interceptors check
    
    * cr fixes
    
    * Update Source/LinqToDB.EntityFrameworkCore/LinqToDBForEFTools.ContextExtensions.cs
    
    Co-authored-by: Svyatoslav Danyliv <sdanyliv@gmail.com>
    
    * Update Source/LinqToDB.EntityFrameworkCore/LinqToDBForEFTools.ContextExtensions.cs
    
    Co-authored-by: Svyatoslav Danyliv <sdanyliv@gmail.com>
    
    * cosmetics
    
    * Make traversing EF core interceptors configurable and disabled by default.
    
    * Update Source/LinqToDB.EntityFrameworkCore/LinqToDBForEFTools.cs
    
    Co-authored-by: Svyatoslav Danyliv <sdanyliv@gmail.com>
    
    * Update Source/LinqToDB.EntityFrameworkCore/LinqToDBForEFTools.ContextExtensions.cs
    
    Co-authored-by: Svyatoslav Danyliv <sdanyliv@gmail.com>
    
    * changed the name of method for using ef core registered interceptors to UseEfCoreRegisteredInterceptorsIfPossible.
    
    * removed UseEfCoreRegisteredInterceptorsIfPossible support and added possibility to create extension having same functionality.
    
    Added sample implementation of such extension method in tests.
    
    * updated README.md
    
    * cosmetic naming change
    
    Co-authored-by: Svyatoslav Danyliv <sdanyliv@gmail.com>
    (cherry picked from commit df56f7f7fefac37eb12c29da19e3f2a08cceff8e)
    
    Co-authored-by: Przemysław Łukawski <przemyslaw.luk@gmail.com>

---
+++ Source/LinqToDB.EntityFrameworkCore/LinqToDBForEFTools.ContextOptionsBuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace LinqToDB.EntityFrameworkCore
+{
+	using Internal;
+
+	public static partial class LinqToDBForEFTools
+	{
+		/// <summary>
+		/// Registers custom options related to LinqToDB provider
+		/// </summary>
+		/// <param name="optionsBuilder"></param>
+		/// <param name="linq2DbOptionsAction">Custom options action</param>
+		/// <returns></returns>
+		public static DbContextOptionsBuilder UseLinqToDb(
+			this DbContextOptionsBuilder optionsBuilder,
+			Action<LinqToDBContextOptionsBuilder>? linq2DbOptionsAction = null)
+		{
+			((IDbContextOptionsBuilderInfrastructure)optionsBuilder)
+				.AddOrUpdateExtension(GetOrCreateExtension(optionsBuilder));
+
+			linq2DbOptionsAction?.Invoke(new LinqToDBContextOptionsBuilder(optionsBuilder));
+
+			return optionsBuilder;
+		}
+
+		private static LinqToDBOptionsExtension GetOrCreateExtension(DbContextOptionsBuilder options)
+			=> options.Options.FindExtension<LinqToDBOptionsExtension>()
+				?? new LinqToDBOptionsExtension();
+	}
+}
