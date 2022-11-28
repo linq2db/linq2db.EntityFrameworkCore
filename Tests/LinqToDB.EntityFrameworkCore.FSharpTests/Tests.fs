97919b4429e1bc2d4eceae3350c66aa0eb1eb697
MaceWindu <MaceWindu@users.noreply.github.com>
2022-11-28 13:56:27 +0100

    add test

--- Tests/LinqToDB.EntityFrameworkCore.FSharpTests/Tests.fs
+++ Tests/LinqToDB.EntityFrameworkCore.FSharpTests/Tests.fs
@@ -1,7 +1,11 @@
 ﻿module LinqToDB.EntityFrameworkCore.FSharpTests
 
+open System
 open System.Linq
 open LinqToDB
+open LinqToDB.Mapping
+open LinqToDB.Data
+open LinqToDB.EntityFrameworkCore.BaseTests
 open System.ComponentModel.DataAnnotations
 open System.ComponentModel.DataAnnotations.Schema
 open Microsoft.EntityFrameworkCore
@@ -16,19 +20,40 @@ type WithIdentity = {
     Name : string
 }
 
+[<CLIMutable>]
+type OptionTable = {
+    [<Key>]
+    Id : int
+    ReferenceOption : string option
+    ValueOption : int option
+    NValueOption : Nullable<int> option
+}
+
 type AppDbContext(options: DbContextOptions<AppDbContext>) =
     inherit DbContext(options)
 
     [<DefaultValue>] val mutable WithIdentity : DbSet<WithIdentity>
+    [<DefaultValue>] val mutable Options : DbSet<OptionTable>
     member this.CompaniesInformation
         with get() = this.WithIdentity
         and set v = this.WithIdentity <- v
+    member this.OptionsTable
+        with get() = this.Options
+        and set v = this.Options <- v
+    override _.OnModelCreating builder = builder.RegisterOptionTypes()
 
 type TestDbContextFactory() =
     member this.CreateDbContext() =
+        //MappingSchema.Default.SetConvertExpression<int option, DataParameter>((fun (x : int option) -> new DataParameter(null, if x.IsSome then x.Value :> Object else null)), false)
+        MappingSchema.Default.SetConverter<int option, DataParameter>((fun (x : int option) -> new DataParameter(null, if x.IsSome then x.Value :> Object else null)))
+        MappingSchema.Default.SetConverter<int, DataParameter>((fun (x : int) -> new DataParameter(null, x)))
         let options = new DbContextOptionsBuilder<AppDbContext>()
+        options.UseLoggerFactory(TestUtils.LoggerFactory) |> ignore
         options.UseSqlite("DataSource=:memory:").UseFSharpTypes() |> ignore
-        new AppDbContext(options.Options)
+        let context = new AppDbContext(options.Options)
+        context.Database.OpenConnection()
+        context.Database.EnsureCreated() |> ignore
+        context
 
 [<TestFixture>]
 type Tests() =
@@ -59,3 +84,42 @@ type Tests() =
                     |}) )
         //q.ToArray() |> ignore
         q.ToLinqToDB().ToString() |> ignore
+
+    [<Test>]
+    member this.TestOption() =
+        let context = TestDbContextFactory().CreateDbContext()
+
+        let p =
+            { ReferenceOption = None
+              Id = 1
+              ValueOption = None
+              NValueOption = None}
+
+        context.CreateLinqToDbContext().Insert(p) |> ignore
+
+        let p =
+            { ReferenceOption = Some "test"
+              Id = 2
+              ValueOption = Some 3
+              NValueOption = Some (Nullable 4) }
+
+        context.CreateLinqToDbContext().Insert(p) |> ignore
+
+        let inserted = query {
+            for p in context.Options do
+            where (p.Id = 1)
+            exactlyOne }
+
+        Assert.AreEqual(None, inserted.ReferenceOption)
+        // this line fails
+        Assert.AreEqual(None, inserted.ValueOption)
+        Assert.AreEqual(None, inserted.NValueOption)
+
+        let inserted = query {
+            for p in context.Options do
+            where (p.Id = 2)
+            exactlyOne }
+
+        Assert.AreEqual(Some "test", inserted.ReferenceOption)
+        Assert.AreEqual(Some 3, inserted.ValueOption)
+        Assert.AreEqual(Some (Nullable 4), inserted.NValueOption)
