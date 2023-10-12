1e29f509962290c550cf27fc4f723071ee69de8f
MaceWindu <MaceWindu@users.noreply.github.com>
2023-10-12 22:48:48 +0200

    handle AsNoTrackingWithIdentityResolution

--- Tests/LinqToDB.EntityFrameworkCore.BaseTests/Models/ForMapping/WithInheritance.cs
+++ Tests/LinqToDB.EntityFrameworkCore.BaseTests/Models/ForMapping/WithInheritance.cs
@@ -1,22 +1,23 @@
-﻿namespace LinqToDB.EntityFrameworkCore.BaseTests.Models.ForMapping;
-
-public class WithInheritance
+﻿namespace LinqToDB.EntityFrameworkCore.BaseTests.Models.ForMapping
 {
-	public int Id { get; set; }
-	public string Discriminator { get; set; } = null!;
-}
+	public class WithInheritance
+	{
+		public int Id { get; set; }
+		public string Discriminator { get; set; } = null!;
+	}
 
-public class WithInheritanceA : WithInheritance
-{
-	
-}
+	public class WithInheritanceA : WithInheritance
+	{
 
-public class WithInheritanceA1 : WithInheritanceA
-{
-	
-}
+	}
 
-public class WithInheritanceA2 : WithInheritanceA
-{
-	
+	public class WithInheritanceA1 : WithInheritanceA
+	{
+
+	}
+
+	public class WithInheritanceA2 : WithInheritanceA
+	{
+
+	}
 }
